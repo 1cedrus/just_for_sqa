409d1abfafa911b929b12a1487a10213c6d59340
tungdt.tt <tungdt.tt@mcredit.com.vn>
2025-04-06 21:53:25 +0700

    add new unit test for repo

---
+++ src/test/java/com/restaurent/manager/repository/PermissionRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.restaurent.manager.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
+class PermissionRepositoryTest {
+    @Autowired
+    PermissionRepository permissionRepository;
+}
