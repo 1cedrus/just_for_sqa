ae5a77c3232b05b02d61353894f716ee8956fe58
tungdz <dttung2113@gmail.com>
2025-04-08 23:50:09 +0700

    Package Service unit test

--- src/main/java/com/restaurent/manager/entity/Permission.java
+++ src/main/java/com/restaurent/manager/entity/Permission.java
@@ -2,6 +2,7 @@ package com.restaurent.manager.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Permission {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
